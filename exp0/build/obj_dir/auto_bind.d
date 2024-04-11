6fcd0fe7daf1a32c4242eb31e5f3b9638062d1c3
Xiao Yang <james_shaw@126.com>
2024-04-11 16:30:45 +0800

    waveform delete before pushing

--- exp0/build/obj_dir/auto_bind.d
+++
@@ -1,8 +0,0 @@
-auto_bind.o: /home/young/digits_experiment/exp0/build/auto_bind.cpp \
- /home/young/ysyx-workbench/nvboard/include/nvboard.h \
- /home/young/ysyx-workbench/nvboard/include/pins.h Vlight.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
